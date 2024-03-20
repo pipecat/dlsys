9babf41b575892136744d8b8048111e448424752
pipecat <pipecat@yahoo.com>
2024-03-21 01:10:55 +0800

    hw2: finish SGD optimizer

--- hw/hw2/python/needle/optim.py
+++ hw/hw2/python/needle/optim.py
@@ -1,4 +1,7 @@
 """Optimization module"""
+from collections import defaultdict
+from email.policy import default
+
 import needle as ndl
 import numpy as np
 
@@ -20,38 +23,23 @@ class SGD(Optimizer):
         super().__init__(params)
         self.lr = lr
         self.momentum = momentum
-        self.u = {}
+        self.u = defaultdict(float)
         self.weight_decay = weight_decay
 
     def step(self):
         ### BEGIN YOUR SOLUTION
-        # self.clip_grad_norm()
         for p in self.params:
-            # It is necessary to check if a parameter has a gradient since it
-            # may not constitute the computational graph. This happens when
-            # multiplying a parameter with 0, which is simply weeded out in
-            # code using branches.
-            if p.grad is None:
+            if p is None or p.grad is None:
                 continue
+            if self.weight_decay > 0:
+                grad = p.grad.data + self.weight_decay * p.data
+            else:
+                grad = p.grad.data
             p_id = id(p)
-            grad = (1. - self.momentum) * (p.grad.data + self.weight_decay * p.data) + \
-                   self.momentum * self.u.get(p_id, 0.)
-            print(grad.shape)
-            p.data -= self.lr * grad
-            self.u[p_id] = grad
-        # for p in self.params:
-        #     if p is None or p.grad is None:
-        #         continue
-        #     if self.weight_decay > 0:
-        #         grad = p.grad.data + self.weight_decay * p.data
-        #     else:
-        #         grad = p.grad.data
-        #     # grad = p.grad.data + self.weight_decay * p.data
-        #     p_id = id(p)
-        #     if p_id not in self.u:
-        #         self.u[p_id] = ndl.init.zeros(*grad.shape, device=grad.device, dtype=grad.dtype)
-        #     self.u[p_id] = self.momentum * self.u[p_id] + (1 - self.momentum) * grad
-        #     p.data = p.data - self.lr * self.u[p_id]
+            if p_id not in self.u:
+                self.u[p_id] = ndl.init.zeros(*grad.shape, device=grad.device, dtype=grad.dtype)
+            self.u[p_id] = self.momentum * self.u[p_id] + (1 - self.momentum) * grad
+            p.data = p.data - self.lr * self.u[p_id]
         ### END YOUR SOLUTION
 
     def clip_grad_norm(self, max_norm=0.25):
@@ -59,23 +47,12 @@ class SGD(Optimizer):
         Clips gradient norm of parameters.
         """
         ### BEGIN YOUR SOLUTION
-        # total_norm = np.linalg.norm(np.array([np.linalg.norm(p.grad.detach().numpy()).reshape((1,)) for p in self.params]))
-        # clip_coef = max_norm / (total_norm + 1e-6)
-        # clip_coef_clamped = min((np.asscalar(clip_coef), 1.0))
-        # for p in self.params:
-        #     p.grad = p.grad.detach() * clip_coef_clamped
-        # total_norm = np.linalg.norm(np.array([np.linalg.norm(p.grad.detach().numpy()).reshape((1,)) for p in self.params]))
-        # clip_coef = max_norm / (total_norm + 1e-6)
-        # clip_coef_clamped = min((np.asscalar(clip_coef), 1.0))
-        # for p in self.params:
-        #     p.grad = p.grad.detach() * clip_coef_clamped
-        pass
 
-        # for p in self.params:
-        #     if p.grad is not None:
-        #         grad_norm = np.linalg.norm(p.grad)
-        #         if grad_norm > max_norm:
-        #             p.grad *= max_norm / grad_norm
+        for p in self.params:
+            if p.grad is not None:
+                grad_norm = np.linalg.norm(p.grad)
+                if grad_norm > max_norm:
+                    p.grad *= max_norm / grad_norm
         ### END YOUR SOLUTION
 
 
@@ -97,8 +74,8 @@ class Adam(Optimizer):
         self.weight_decay = weight_decay
         self.t = 0
 
-        self.m = {}
-        self.v = {}
+        self.m = defaultdict(float)
+        self.v = defaultdict(float)
 
     def step(self):
         ### BEGIN YOUR SOLUTION
